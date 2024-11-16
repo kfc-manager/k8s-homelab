98ec5e7967d2d2c6cf57a69cbdba4b214121c098
kfc-manager <kilianc0709@gmail.com>
2024-11-17 00:25:52 +0100

    fixed kubernetes setup and added argocd

--- cleanup.sh
+++
@@ -1,5 +0,0 @@
-sudo rm -rf /etc/kubernetes/manifests/kube-apiserver.yaml
-sudo rm -rf /etc/kubernetes/manifests/kube-controller-manager.yaml
-sudo rm -rf /etc/kubernetes/manifests/kube-scheduler.yaml
-sudo rm -rf /etc/kubernetes/manifests/etcd.yaml
-sudo systemctl stop kubelet.service
